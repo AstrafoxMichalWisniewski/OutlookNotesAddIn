22fce8140cdc75872ec3a5218bf4e8d48d23c04e
Michał Wiśniewski <michal.wisniewski@astrafox.pl>
2025-05-21 07:37:40 +0200

    fix

--- src/taskpane/secrets.ts
+++ src/taskpane/secrets.ts
@@ -1,4 +1,1 @@
 // secrets.ts
-// Replace the value below with your actual OneDrive client ID
-export const ONEDRIVE_CLIENT_ID = "548b6512-4a59-428d-97b7-3a31f1533413";
-//export const ONEDRIVE_CLIENT_ID = "YOUR_ONEDRIVE_CLIENT_ID_HERE";
