78cef99a039cde59cdfa704e0ece247e9bee8e3f
Michał Wiśniewski <michal.wisniewski@astrafox.pl>
2025-07-01 09:16:15 +0200

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const CopyWebpackPlugin = require("copy-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 const urlDev = "https://localhost:3000/";
-const urlProd = "https://delightful-grass-02770d803.6.azurestaticapps.net/"; // CHANGE THIS TO YOUR PRODUCTION DEPLOYMENT LOCATION
+const urlProd = "https://astrafoxmichalwisniewski.github.io/OutlookNotesAddIn/"; // CHANGE THIS TO YOUR PRODUCTION DEPLOYMENT LOCATION
 
 async function getHttpsOptions() {
   const httpsOptions = await devCerts.getHttpsServerOptions();
